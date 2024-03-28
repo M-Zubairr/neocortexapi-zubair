268edd6845942a86db1e4e778620e2b31e975930
Syeda Ayesha Asad <148999818+SyedaAyeshaAsad@users.noreply.github.com>
2024-03-29 01:53:05 +0500

    Added class for holding the read information

---
+++ source/Samples/EnhanceMultisequenceLearning/Data/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnhanceMultisequenceLearning.Data
+{
+    public class Report
+    {
+        public Report() { }
+
+        public string SequenceName { get; set; }
+        public int[] SequenceData { get; set; }
+        public List<string> PredictionLog { get; set; }
+        public double Accuracy { get; set; }
+
+    }
+}
